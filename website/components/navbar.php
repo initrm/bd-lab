38d923db68225a21f0b1d7facce1d34760ea8977
Andrea Tinelli <tinelliandrea2@outlook.it>
2023-06-04 11:33:56 +0200

    Aggiunto margine inferiore alla navbar

--- website/components/navbar.php
+++ website/components/navbar.php
@@ -1,5 +1,5 @@
 <?php function navbar($display_name) { ?>
-  <nav class="navbar is-transparent" style="background-color: #e9e9e9;" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-transparent mb-3" style="background-color: #e9e9e9;" role="navigation" aria-label="main navigation">
     <div class="navbar-menu">
       <div class="navbar-start">
         <p class="navbar-item">Loggato come: <span class="has-text-link"><?php echo $display_name; ?></span></p>
