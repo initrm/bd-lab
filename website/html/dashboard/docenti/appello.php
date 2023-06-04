0743f5e50ef3a9accaa84e1bc34a3424c37a16b0
Andrea Tinelli <tinelliandrea2@outlook.it>
2023-06-04 23:10:14 +0200

    Aggiunta pagina per gestire gli esami di un appello

---
+++ website/html/dashboard/docenti/appello.php
@@ -0,0 +1,156 @@
+<?php
+  error_reporting(E_ERROR | E_PARSE);
+
+  include_once('./../../../auth.php');
+  include_once('./../../../utype.php');
+  include_once('./../../../components/head.php');
+  include_once('./../../../database.php');
+  include_once('./../../../components/navbar.php');
+
+  $authenticator = new Authenticator();
+
+  // se l'utente non è loggato viene reindirizzato alla pagina per effettuare l'accesso
+  if(!$authenticator->is_authenticated()) {
+    header("location: /login.php");
+    die();
+  }
+
+  // se l'utente non ha i permessi per accedere a questa pagina viene reindirizzato alla sua dashboard
+  if($authenticator->get_authenticated_user_type() != UserType::Docente) {
+    header("location: /dashboard/index.php");
+    die();
+  }
+
+  // apertura connessione con il database
+  $database = new Database();
+  $database->open_conn();
+
+  // eventuale messaggio di errore
+  $error_msg = NULL;
+  
+  // ottenimento dell'appello e dell'insegnamento
+  // ok quando esiste l'appello, esiste l'insegnamento ed è di proprietà dell'utente loggato
+  $query_string = "
+    select * 
+    from appelli a
+    inner join insegnamenti i on a.insegnamento = i.codice and a.corso_laurea = i.corso_laurea
+    where a.id = $1 and i.docente = $2
+  ";
+  $query_params = array($_GET["id"], $authenticator->get_authenticated_user()["email"]);
+  $result = $database->execute_query("get_appello_join_insegnamento", $query_string, $query_params);
+  // se l'appello non esiste, l'utente viene reindirizzato alla index della dashboard
+  if($result->row_count() == 0) {
+    header("location: /dashboard/docenti/index.php");
+    $database->close_conn();
+    die();
+  }
+  $appello = $result->row();
+
+  // ottenimento studenti iscritti all'appello
+  $query_string = "select * from get_iscrizioni_ad_appello($1)";
+  $query_params = array($appello["id"]);
+  $result = $database->execute_query("get_iscrizioni_appello", $query_string, $query_params);
+  $studenti = $result->all_rows();
+
+  // chiusura connessione
+  $database->close_conn();
+
+?>
+<!DOCTYPE html>
+<html>
+  <?php head("Gestione Iscrizioni Appello"); ?>
+  <body>
+    <?php 
+      $user = $authenticator->get_authenticated_user();
+      $display_name = $user["nome"] . " " . $user["cognome"];
+      navbar($display_name);
+    ?>
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-10-desktop">
+          <div class="columns mx-2 is-multiline">
+
+            <!-- titolo pagina e sottotitolo -->
+            <div class="column is-12">
+              <h1 class="title is-1">Gestione iscrizioni appello</h1>
+              <h2 class="subtitle">
+                Gestisci le iscrizioni per l'appello in data 
+                <?php echo date_format(date_create($appello["data"]), "d/m/Y"); ?> 
+                di "<?php echo $appello["nome"]; ?>"
+              </h2>
+            </div>
+
+            <!-- breadcrumb -->
+            <div class="column is-12">
+              <nav class="breadcrumb" aria-label="breadcrumbs">
+                <ul>
+                  <li><a href="/dashboard/docenti/index.php">Home</a></li>
+                  <li><a href="/dashboard/docenti/appelli.php?insegnamento=<?php echo $appello["codice"]; ?>&cdl=<?php echo $appello["corso_laurea"]; ?>">Appelli di "<?php echo $appello["nome"]; ?>"</a></li>
+                  <li class="is-active">
+                    <a href="#" aria-current="page">Appello in data <?php echo date_format(date_create($appello["data"]), "d/m/Y"); ?></a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <!-- card appelli -->
+            <?php for($i = 0; $i < sizeof($studenti); $i++) { ?>
+              <div class="column is-12">
+                <div class="box">
+                  <div class="columns">
+                    <div class="column is-8 is-flex is-align-items-center">
+                      <p>
+                        <?php echo $studenti[$i]["nome_studente"] . " " . $studenti[$i]["cognome_studente"] . ", Matricola: " . $studenti[$i]["matricola_studente"]; ?>
+                      </p>
+                    </div>
+                    <div class="column is-2 is-flex is-justify-content-end is-align-items-center">
+                        <div class="control">
+                          <input 
+                            id="valutazione-s-<?php echo $studenti[$i]["matricola_studente"]; ?>-a-<?php echo $appello["id"]; ?>"
+                            name="valutazione"
+                            min="0"
+                            max="30"
+                            minlength="6"
+                            class="input"
+                            type="number"
+                            placeholder="Valutazione"
+                            <?php
+                              if($studenti[$i]["valutazione_esame"] != NULL)
+                                echo "value=\"" . $studenti[$i]["valutazione_esame"] . "\"";
+                            ?>
+                          />
+                        </div>
+                    </div>
+                    <div class="column is-2 is-flex is-justify-content-end">
+                      <button class="button is-link is-outlined is-fullwidth" onclick="<?php echo "salvaValutazione(" . $appello["id"] . ", " . $studenti[$i]["matricola_studente"] . ")"; ?>">
+                        <ion-icon name="save-outline"></ion-icon>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <?php } ?>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- axios lib -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- ionicons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <!-- toast -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    <script type="text/javascript" src="/scripts/toast.js"></script>
+    <!-- funzione salva valutazione -->
+    <script>
+      function salvaValutazione(appello, studente) {
+        var element = document.getElementById("valutazione-s-" + studente + "-a-" + appello);
+        axios.postForm("/api/docenti/save-result.php", { appello, studente, valutazione: element.value })
+          .then(({ data }) => showSuccessToast(data.message))
+          .catch(({ response: { data }}) => showDangerToast(data.message));
+      }
+    </script>
+  </body>
+</html>
