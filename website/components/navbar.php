6713f1c9a2cb266e7b305ed37a5f4207862467da
Andrea Tinelli <tinelliandrea2@outlook.it>
2023-06-05 08:57:12 +0200

    Resa navbar responsive

--- website/components/navbar.php
+++ website/components/navbar.php
@@ -1,6 +1,15 @@
 <?php function navbar($display_name) { ?>
   <nav class="navbar is-transparent mb-3" style="background-color: #e9e9e9;" role="navigation" aria-label="main navigation">
-    <div class="navbar-menu">
+    
+    <!-- burger solo mobile -->
+    <a role="button" id="burger-button" class="navbar-burger" data-target="navbar" aria-label="menu" aria-expanded="false">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+
+    <!-- elementi navbar -->
+    <div class="navbar-menu" id="navbar">
       <div class="navbar-start">
         <p class="navbar-item">Loggato come: <span class="has-text-link"><?php echo $display_name; ?></span></p>
       </div>
@@ -14,4 +23,19 @@
       </div>
     </div>
   </nav>
+
+  <!-- script espansione menu mobile -->
+  <script type="text/javascript">
+    document.addEventListener('DOMContentLoaded', () => {
+      const burgerButton = document.getElementById('burger-button');
+      burgerButton.addEventListener('click', () => {
+        // ottenimento del target dall'attributo "data-target"
+        const target = burgerButton.dataset.target;
+        const $target = document.getElementById(target);
+        // attivazione
+        burgerButton.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  </script>
 <?php } ?>
