a8b52a00aa68ff707d2c61f0431eb5168b3430b9
Andrea Tinelli <tinelliandrea2@outlook.it>
2023-06-07 15:15:56 +0200

    Aggiornato componente select docenti per supportare valore di default

--- website/components/select-docenti.php
+++ website/components/select-docenti.php
@@ -4,9 +4,12 @@
   /**
    * restituisce un componente html che permette di selezionare i docenti tra quelli disponibili, che 
    * come attributo name ha "docente" e che come value di ciascun opzione ha l'id (email) di ciascun docente
+   * 
+   * $default_value deve essere una stringa contenente l'email del docente che deve essere selezionato di default
    */
-  function select_docenti(Database $database) {
+  function select_docenti(Database $database, ?string $default_value = NULL) {
 
+    // ottenimento docenti
     $query_string = "select email, nome, cognome from docenti";
     $results = $database->execute_query("select_docenti_component_get_docenti", $query_string, array());
     $docenti = $results->all_rows();
@@ -17,7 +20,9 @@
         <div class="select is-fullwidth">
           <select name="docente">
             <?php foreach($docenti as $docente) { ?>
-              <option value="<?php echo $docente["email"]; ?>"><?php echo $docente["nome"] . " " . $docente["cognome"]; ?></option>
+              <option <?php if($default_value != NULL && $docente["email"] == $default_value) { ?>selected="selected"<?php } ?>value="<?php echo $docente["email"]; ?>">
+                <?php echo $docente["nome"] . " " . $docente["cognome"]; ?>
+              </option>
             <?php } ?>
           </select>
         </div>
