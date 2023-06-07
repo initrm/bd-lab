a090c7c05be4a9b010bc6933886a51c6a09c098f
Andrea Tinelli <tinelliandrea2@outlook.it>
2023-06-07 14:22:29 +0200

    Aggiunto componente title

---
+++ website/components/title.php
@@ -0,0 +1,10 @@
+<?php function section_title(?string $title, ?string $subtitle) { ?>
+  <div class="column is-12">
+    <?php if($title != NULL) { ?>
+      <h1 class="title is-1"><?php echo $title; ?></h1>
+    <?php } ?>
+    <?php if($subtitle != NULL) { ?>
+      <h2 class="subtitle"><?php echo $subtitle; ?></h2>
+    <?php } ?>
+  </div>
+<?php } ?>
