33cc051ded6a68da0c5b53cfde0582a52b123307
Andrea Tinelli <tinelliandrea2@outlook.it>
2023-06-07 14:58:44 +0200

    Aggiornata pagina index dashboard segreteria per sfruttare nuovi componenti

--- website/html/dashboard/segreteria/index.php
+++ website/html/dashboard/segreteria/index.php
@@ -2,8 +2,9 @@
   include_once('./../../../auth.php');
   include_once('./../../../utype.php');
   include_once('./../../../components/head.php');
-  include_once('./../../../database.php');
   include_once('./../../../components/navbar.php');
+  include_once('./../../../components/title.php');
+  include_once('./../../../components/segreteria/menu-item-card.php');
 
   $authenticator = new Authenticator();
 
@@ -23,70 +24,29 @@
 <html>
   <?php head("Dashboard Segreteria"); ?>
   <body>
-    <?php 
-      $user = $authenticator->get_authenticated_user();
-      $display_name = $user["email"];
-      navbar($display_name);
-    ?>
+
+    <!-- navbar -->
+    <?php navbar($authenticator->get_authenticated_user()["email"]); ?>
+
+    <!-- content -->
     <div class="container">
       <div class="columns is-centered">
         <div class="column is-10-desktop">
-
           <div class="columns mx-2 is-multiline">
 
             <!-- titolo pagina e sottotitolo -->
-            <div class="column is-12">
-              <h1 class="title is-1">Dashboard</h1>
-              <h2 class="subtitle">Gestisci studenti, docenti, cdl ed insegnamenti</h2>
-            </div>
+            <?php section_title("Dashboard", "Gestisci studenti, docenti, cdl ed insegnamenti"); ?>
 
             <!-- gestione studenti -->
-            <div class="column is-6">
-              <div class="card mb-5">
-                <header class="card-header">
-                  <p class="card-header-title">Gestione studenti</p>
-                </header>
-                <div class="card-content">
-                  <div class="content">Visualizza, aggiungi e rimuovi studenti.</div>
-                </div>
-                <footer class="card-footer">
-                  <a href="/dashboard/segreteria/studenti.php" class="card-footer-item">Vai</a>
-                </footer>
-              </div>
-            </div>
+            <?php menu_item_card("Gestione studenti", "Visualizza, aggiungi e rimuovi studenti.", "/dashboard/segreteria/studenti.php"); ?>
 
             <!-- gestione studenti -->
-            <div class="column is-6">
-              <div class="card mb-5">
-                <header class="card-header">
-                  <p class="card-header-title">Gestione docenti</p>
-                </header>
-                <div class="card-content">
-                  <div class="content">Visualizza, aggiungi e rimuovi docenti.</div>
-                </div>
-                <footer class="card-footer">
-                  <a href="/dashboard/segreteria/docenti.php" class="card-footer-item">Vai</a>
-                </footer>
-              </div>
-            </div>
+            <?php menu_item_card("Gestione docenti", "Visualizza, aggiungi e rimuovi docenti.", "/dashboard/segreteria/docenti.php"); ?>
 
             <!-- gestione cdl -->
-            <div class="column is-6">
-              <div class="card mb-5">
-                <header class="card-header">
-                  <p class="card-header-title">Gestione corsi di laurea</p>
-                </header>
-                <div class="card-content">
-                  <div class="content">Visualizza, aggiungi e rimuovi corsi di laurea.</div>
-                </div>
-                <footer class="card-footer">
-                  <a href="/dashboard/segreteria/corsi-laurea.php" class="card-footer-item">Vai</a>
-                </footer>
-              </div>
-            </div>
+            <?php menu_item_card("Gestione corsi di laurea", "Visualizza, aggiungi e rimuovi corsi di laurea.", "/dashboard/segreteria/corsi-laurea.php"); ?>
 
           </div>
-
         </div>
       </div>
     </div>
