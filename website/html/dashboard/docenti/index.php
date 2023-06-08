0528f4b4d5774c33127d8a7b92899d55ed13e1fc
Andrea Tinelli <tinelliandrea2@outlook.it>
2023-06-08 19:20:47 +0200

    Riorganizzato backend pagina index dashboard docenti

--- website/html/dashboard/docenti/index.php
+++ website/html/dashboard/docenti/index.php
@@ -19,6 +19,14 @@
     header("location: /dashboard/index.php");
     die();
   }
+
+  // ottenimento insegnamenti del docente
+  $database = new Database();
+  $query_string = "select * from insegnamenti where docente = $1";
+  $query_params = array($authenticator->get_authenticated_user()["email"]);
+  $result = $database->execute_single_query($query_string, $query_params);
+  $insegnamenti = $result->all_rows();
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -41,34 +49,25 @@
             <?php section_title("I tuoi insegnamenti", "Gestisci i calendari d'esame dei tuoi insegnamenti"); ?>
 
             <!-- card insegnamenti -->
-            <?php
-              $database = new Database();
-              $query_string = "select * from insegnamenti where docente = $1";
-              $query_params = array($authenticator->get_authenticated_user()["email"]);
-              $result = $database->execute_single_query($query_string, $query_params);
-              $rows = $result->all_rows();
-              for($i = 0; $i < sizeof($rows); $i++) {
-            ?>
+            <?php foreach($insegnamenti as $insegnamento) { ?>
               <div class="column is-12">
                 <div class="card mb-5">
                   <header class="card-header">
                     <p class="card-header-title">
-                      <?php echo $rows[$i]["nome"] ?>
+                      <?php echo $insegnamento["nome"] ?>
                     </p>
                   </header>
                   <div class="card-content">
                     <div class="content">
-                      <?php echo $rows[$i]["descrizione"] ?>
+                      <?php echo $insegnamento["descrizione"] ?>
                     </div>
                   </div>
                   <footer class="card-footer">
-                    <a href="/dashboard/docenti/appelli.php?insegnamento=<?php echo $rows[$i]["codice"] ?>&cdl=<?php echo $rows[$i]["corso_laurea"] ?>" class="card-footer-item">Visualizza appelli</a>
+                    <a href="/dashboard/docenti/appelli.php?insegnamento=<?php echo $insegnamento["codice"] ?>&cdl=<?php echo $insegnamento["corso_laurea"] ?>" class="card-footer-item">Visualizza appelli</a>
                   </footer>
                 </div>
               </div>
-            <?php
-              }
-            ?>
+            <?php } ?>
 
           </div>
         </div>
